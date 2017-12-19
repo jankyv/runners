518f3a1b7ea299e91d90ddd132f30a05a7e91cc8
jankyv <jancornevink@gmail.com>
2017-12-19 11:44:21 +0100

    Add android design Add track/training

---
+++ runningapp/runningapp/Track.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Locations;
+
+namespace Running
+{
+    class Track
+    {
+        private int TrackId;
+        private List<Location> locationList;
+
+        public List<Location> LocationList { get => locationList; set => locationList = value; }
+
+        public void AddPoint(Location p)
+        {
+            LocationList = new List<Location>();
+            LocationList.Add(p);
+            Console.WriteLine("GIT");
+        }
+    }
+}
---
+++ runningapp/runningapp/Training.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Locations;
+
+namespace Running
+{
+    class Training
+    {
+        private List<Track> tracks;
+
+
+        public Training()
+        {
+            Tracks = new List<Track>();
+            Tracks.Add(new Track());
+        }
+
+        internal List<Track> Tracks { get => tracks; set => tracks = value; }
+
+        public void AddPoint(Location p)
+        {
+            Tracks[Tracks.Count-1].AddPoint(p);
+        }
+
+        public void PauseTraining()
+        {
+            Tracks.Add(new Track());
+        }
+
+        public Track CurrentTrack()
+        {
+            return Tracks[Tracks.Count - 1];
+        }
+    }
+}
